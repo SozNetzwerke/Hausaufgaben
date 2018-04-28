410c46899489cb4305143e0ec8101df4e75b0a83
ryanmarcwegmeier <ryan.wegmeier@gmail.com>
2018-04-28 10:37:53 +0200

    Ha1 fin

---
+++ HA1/src/progress.js
@@ -0,0 +1,19 @@
+
+function setProgress(value,id){
+    var x=value;
+    if (x==""){
+        $(id).val(0);
+        x=0;
+    }
+    if(x>=0 && x<=100){
+        $(id).text(x.toString()+"%")
+            $(id).css("width",x.toString()+"%");
+    }else{
+            $(id).css("width","0%");
+    }
+}
+
+function reProg(){
+    alert();
+    $("#input_progress").css("width",0);
+}
